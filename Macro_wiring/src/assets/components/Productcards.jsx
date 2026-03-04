bba008dc047a1c398c04ccad32422dc4d5d6d8ad
John Paul Valdez <valconfi1212@gmail.com>
2026-03-04 08:18:53 +0800

    Some changes

--- Macro_wiring/src/assets/components/Productcards.jsx
+++ Macro_wiring/src/assets/components/Productcards.jsx
@@ -15,9 +15,6 @@ const ProductCard = ({ name, description, image }) => {
         <h3 className="text-lg font-semibold mb-2">{name}</h3>
         <p className="text-gray-600 text-sm mb-4">{description}</p>
 
-        <button className="bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700 transition">
-          View Details
-        </button>
       </div>
     </div>
   );
